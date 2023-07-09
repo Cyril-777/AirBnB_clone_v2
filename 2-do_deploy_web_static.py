925d8022abe218cb933a882e1a7fe51ad4d7dbfa
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-07-09 04:13:48 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
 """Compress web static package
 """
-from fabric.api import put, run, env
-from os import path
+from fabric.api import env, put, run
+from os.path import exists
+from datetime import datetime
 
 env.hosts = ['54.173.134.61', '54.89.133.85']
-env.user = 'ubuntu'
 
 def do_deploy(archive_path):
     """
     Distributes an archive to the web servers.
     """
-    if not path.exists(archive_path):
+    if not exists(archive_path):
         return False
 
     try:
-        # uploads the archive to the /tmp/ directory of the web server
-        put(archive_path, '/tmp/')
+        file_name = archive_path.split('/')[-1]
+        no_ext_name = file_name.split('.')[0]
+        remote_path = "/data/web_static/releases/{}".format(no_ext_name)
 
-        # uncompress the archive to the folder releases/<archive
-        # filename without extension> on the web server
-        file = archive_path.split('/')[-1]
-        folder = '/data/web_static/releases/' + file.split('.')[0]
-        run('sudo mkdir -p {}'.format(folder))
-        run('sudo tar -xzf /tmp/{} -C {}'.format(file, folder))
+        # uploads the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/{}".format(file_name))
+
+        # uncompress the archive to the folder on the web server
+        run("mkdir -p {}".format(remote_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, remote_path))
 
         # deletes the archive from the web server
-        run('sudo rm /tmp/{}'.format(file))
+        run("rm /tmp/{}".format(file_name))
 
         # moves the content of the uncompressed folder to the proper location
-        run('sudo mv {}/web_static/* {}/'.format(folder, folder))
-        run('sudo rm -rf {}/web_static'.format(folder))
+        run("mv {}/web_static/* {}/".format(remote_path, remote_path))
+        run("rm -rf {}/web_static".format(remote_path))
 
         # delets the symbolic link /data/web_static/current from webserver
-        run('sudo rm -rf /data/web_static/current')
+        run("rm -rf /data/web_static/current")
 
         # creates a new symbolic link /data/web_static/current on webserver
-        run('sudo ln -s {} /data/web_static/current'.format(folder))
-        return True
+        run("ln -s {} /data/web_static/current".format(remote_path))
 
+        return True
     except Exception as e:
         return False

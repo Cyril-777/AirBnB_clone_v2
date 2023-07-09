05f2e38888be68ed68bd60d38bad138520a7120d
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-07-09 04:27:01 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,44 +1,54 @@
 #!/usr/bin/python3
 """Compress web static package
 """
-from fabric.api import env, put, run
-from os.path import exists
+from fabric.api import *
 from datetime import datetime
+from os import path
+
 
 env.hosts = ['54.173.134.61', '54.89.133.85']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to the web servers.
-    """
-    if not exists(archive_path):
-        return False
+        """Distributes an archive to the web servers.
+        """
+        try:
+                if not (path.exists(archive_path)):
+                        return False
 
-    try:
-        file_name = archive_path.split('/')[-1]
-        no_ext_name = file_name.split('.')[0]
-        remote_path = "/data/web_static/releases/{}".format(no_ext_name)
+                # uploads the archive to /tmp/ directory of the web server
+                put(archive_path, '/tmp/')
 
-        # uploads the archive to the /tmp/ directory of the web server
-        put(archive_path, "/tmp/{}".format(file_name))
+                # create target dir
+                file_name = archive_path[-18:-4]
+                run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(file_name))
 
-        # uncompress the archive to the folder on the web server
-        run("mkdir -p {}".format(remote_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, remote_path))
+                # uncompress the archive to the folder on the web server
+                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+/data/web_static/releases/web_static_{}/'
+                    .format(file_name, file_name))
 
-        # deletes the archive from the web server
-        run("rm /tmp/{}".format(file_name))
+                # deletes the archive from the web server
+                run('sudo rm /tmp/web_static_{}.tgz'.format(file_name))
 
-        # moves the content of the uncompressed folder to the proper location
-        run("mv {}/web_static/* {}/".format(remote_path, remote_path))
-        run("rm -rf {}/web_static".format(remote_path))
+                # moves the content of the uncompressed folder
+                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(file_name, file_name))
 
-        # delets the symbolic link /data/web_static/current from webserver
-        run("rm -rf /data/web_static/current")
+                # remove extraneous web_static dir
+                run('sudo rm -rf /data/web_static/releases/\
+web_static_{}/web_static'
+                    .format(file_name))
 
-        # creates a new symbolic link /data/web_static/current on webserver
-        run("ln -s {} /data/web_static/current".format(remote_path))
+                # delets the sym link /data/web_static/current from webserver
+                run('sudo rm -rf /data/web_static/current')
 
+                # creates a new symbolic link /data/web_static/current on webserver
+                run('sudo ln -s /data/web_static/releases/\
+web_static_{}/ /data/web_static/current'.format(file_name))
+        except:
+                return False
         return True
-    except Exception as e:
-        return False

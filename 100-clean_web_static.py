5024eedc5a2e34d67075b527886069d2ef806848
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-07-09 22:52:45 +0300

    Create 100-clean_web_static.py

---
+++ 100-clean_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import os
+from fabric.api import *
+
+env.hosts = ['54.173.134.61', '54.89.133.85']
+
+
+def do_clean(number=0):
+    """deletes out-of-date archives
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]

061e005e2a2f8bdb1c5de9e121feb2743516b81a
Cyril-777 <kyrillosmaher10@gmail.com>
2023-07-22 11:13:45 +0000

    pep8 fix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,18 +9,18 @@ app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def hello():
     """returns a string"""
     return ("Hello HBNB!")
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """returns a string"""
     return ("HBNB")
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def c_with_text(text):
-    """display “C ”, followed by the value of the text variable"""
+    """display C, followed by the value of the text variable"""
     return "C {}".format(text.replace("_", " "))
 
 if __name__ == "__main__":

abd2d69d6222d9d77341a1612d49dcc7edb78e1e
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-07-22 16:28:45 +0300

    Update 5-number_template.py - import render_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,7 +2,7 @@
 """ Write a script that starts a Flask web application:
 Your web application must be listening on 0.0.0.0, port 5000
 """
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
